1253fb977247ea650ddc926c4f4f4927b98a0d5e
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2010-09-15 21:21:48 +0200

    Start preferences UI

---
+++ org.maven.ide.eclipse.gdt/org.maven.ide.eclipse.gdt.core/src/main/java/org/maven/ide/eclipse/gdt/core/properties/MavenGWTPropertyPage.java
@@ -0,0 +1,163 @@
+package org.maven.ide.eclipse.gdt.core.properties;
+
+
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.PropertyPage;
+import org.maven.ide.eclipse.gdt.MavenGdtPlugin;
+import org.osgi.service.prefs.BackingStoreException;
+import org.osgi.service.prefs.Preferences;
+
+public class MavenGWTPropertyPage extends PropertyPage {
+
+	private static final String PATH_TITLE = "Path:";
+	private static final String OWNER_TITLE = "&Owner:";
+	private static final String OWNER_PROPERTY = "OWNER";
+	private static final String DEFAULT_OWNER = "John Doe";
+
+	private static final int TEXT_FIELD_WIDTH = 50;
+
+	private Text ownerText;
+	
+	private Button customProperty;
+
+	/**
+	 * Constructor for SamplePropertyPage.
+	 */
+	public MavenGWTPropertyPage() {
+		super();
+		
+	}
+
+	private void addFirstSection(Composite parent) {
+		Composite composite = createDefaultComposite(parent);
+
+		Label customPropertyLabel = new Label(composite, SWT.NONE);
+		customPropertyLabel.setText("Custom property for this project");
+		
+		customProperty = new Button(composite, SWT.CHECK);
+		
+		//Label for path field
+		Label pathLabel = new Label(composite, SWT.NONE);
+		pathLabel.setText(PATH_TITLE);
+
+		// Path text field
+		Text pathValueText = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+		pathValueText.setText(((IResource) getElement()).getFullPath().toString());
+	}
+
+	private void addSeparator(Composite parent) {
+		Label separator = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
+		GridData gridData = new GridData();
+		gridData.horizontalAlignment = GridData.FILL;
+		gridData.grabExcessHorizontalSpace = true;
+		separator.setLayoutData(gridData);
+	}
+
+	private void addSecondSection(Composite parent) {
+		Composite composite = createDefaultComposite(parent);
+
+		// Label for owner field
+		Label ownerLabel = new Label(composite, SWT.NONE);
+		ownerLabel.setText(OWNER_TITLE);
+
+		// Owner text field
+		ownerText = new Text(composite, SWT.SINGLE | SWT.BORDER);
+		GridData gd = new GridData();
+		gd.widthHint = convertWidthInCharsToPixels(TEXT_FIELD_WIDTH);
+		ownerText.setLayoutData(gd);
+
+		// Populate owner text field
+		try {
+			String owner =
+				((IResource) getElement()).getPersistentProperty(
+					new QualifiedName("", OWNER_PROPERTY));
+			ownerText.setText((owner != null) ? owner : DEFAULT_OWNER);
+		} catch (CoreException e) {
+			ownerText.setText(DEFAULT_OWNER);
+		}
+	}
+
+	/**
+	 * @see PreferencePage#createContents(Composite)
+	 */
+	protected Control createContents(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		composite.setLayout(layout);
+		GridData data = new GridData(GridData.FILL);
+		data.grabExcessHorizontalSpace = true;
+		composite.setLayoutData(data);
+
+		addFirstSection(composite);
+		addSeparator(composite);
+		addSecondSection(composite);
+		
+		Preferences preferences = getPrefences((IProject) getElement(), MavenGdtPlugin.PLUGIN_ID);
+		preferences.put("zozo", "zozo");
+		try {
+			preferences.sync();
+		} catch (BackingStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return composite;
+	}
+
+	private Composite createDefaultComposite(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		composite.setLayout(layout);
+
+		GridData data = new GridData();
+		data.verticalAlignment = GridData.FILL;
+		data.horizontalAlignment = GridData.FILL;
+		composite.setLayoutData(data);
+
+		return composite;
+	}
+
+	protected void performDefaults() {
+		super.performDefaults();
+		// Populate the owner text field with the default value
+		ownerText.setText(DEFAULT_OWNER);
+	}
+	
+	public boolean performOk() {
+		// store the value in the owner text field
+		try {
+			((IResource) getElement()).setPersistentProperty(
+				new QualifiedName("", OWNER_PROPERTY),
+				ownerText.getText());
+		} catch (CoreException e) {
+			return false;
+		}
+		return true;
+	}
+	
+	protected Preferences getPrefences(IProject project, String pref) {
+		if(project==null){
+			return null;
+		}
+		return Platform
+		.getPreferencesService()
+		.getRootNode()
+		.node("project/"+project.getName()+"/" + pref);
+	}
+
+}
