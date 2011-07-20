edac481887a53a66cb3489a02f8ab14e2c399ef1
olivier.nouguier <olivier.nouguier@gmail.com>
2011-07-20 20:13:15 +0200

    Add support for css goal.

---
+++ org.maven.ide.eclipse.gdt/org.maven.ide.eclipse.gdt.core/src/main/java/org/maven/ide/eclipse/gdt/gwt/build/GWTCSSBuildParticipant.java
@@ -0,0 +1,29 @@
+package org.maven.ide.eclipse.gdt.gwt.build;
+
+import java.io.File;
+
+import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GWTCSSBuildParticipant extends GWTBuildParticipant {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GWTCSSBuildParticipant.class);
+
+    public GWTCSSBuildParticipant(IMavenProjectFacade projectFacade, MojoExecution execution, IPluginExecutionMetadata executionMetadata) {
+        super(execution, true);
+    }
+
+  
+    @Override
+    protected boolean fileConcerned(File statedFolder, String file) {
+        if (file.endsWith(".css")) {
+            LOGGER.debug(file + " triggers the build");
+            return true;
+        }
+        return false;
+    }
+
+}
